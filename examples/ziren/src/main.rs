2907afe95f4522bd531104b6de7d9099358d1b37
koukyosyumei <koukyosyumei@hotmail.com>
2025-11-02 23:56:46 -0500

    update

--- examples/ziren/src/main.rs
+++ examples/ziren/src/main.rs
@@ -30,61 +30,23 @@ pub fn add_program(pc_start: u32, pc_base: u32) -> Program {
     Program::new(instructions, pc_start, pc_base)
 }
 
-fn main() -> Result<(), ()> {
-    // ############### Global Parameters #################################
-    let prime = 2_u32.pow(31) - 2_u32.pow(24) + 1;
-    let program_cols = (8..20).collect::<Vec<_>>();
-
-    // ############### Gather Constraints ################################
-    let air = CpuChip::default();
-    let symbolic_constraints: Vec<SymbolicExpression<Mersenne31>> =
-        get_symbolic_constraints(&air, 0, ZKM_PROOF_NUM_PV_ELTS);
-    let tv_constraints = symbolic_constraints
-        .iter()
-        .map(|sc| convert_p3_expr::<Mersenne31>(&sc))
-        .collect::<Vec<_>>();
-
-    // # Additional Public Value Verification
-    let (pv_pos_constraints, pv_neg_constraints) = get_pv_constraints();
-
-    // # Gather Symbolic Constraints
-    let constraints = LatticeVMConstraints {
-        air_constraints: tv_constraints.clone(),
-        pv_pos_constraints,
-        pv_neg_constraints,
-    };
-
-    // ############### Construct SMT formula ############################
-    //let smt = expr_to_smt(&constraints, 1, 68, prime);
-    //println!("{}", smt);
-
-    // ############### Preparation of Solver ############################
-    let mut rng = StdRng::seed_from_u64(42);
-    let max_row_id = 0;
-    let num_extracted_rows = 2;
+pub fn run_solver<FinalCheckFn>(
+    constraints: &LatticeVMConstraints,
+    target_cols: &Vec<usize>,
+    potential_boolean_vars: &Vec<usize>,
+    refinment_target_indicies_pv: &Vec<usize>,
+    base_abs_main_trace_data: &Vec<Vec<AbstractInterval>>,
+    public_vals: Vec<AbstractInterval>,
+    max_row_id: usize,
+    final_check: FinalCheckFn,
+    prime: u32,
+    seed: u64,
+) where
+    FinalCheckFn: Fn(&AbstractTrace, u32),
+{
+    let mut rng = StdRng::seed_from_u64(seed);
     let mut found_solution_flag = false;
-    let potential_boolean_vars = gather_boolean_variables(&tv_constraints);
 
-    // ############### Prepare Public Values ############################
-    let mut public_vals = vec![AbstractInterval::zero(); ZKM_PROOF_NUM_PV_ELTS];
-    public_vals[40] = AbstractInterval::i4();
-    public_vals[41] = AbstractInterval::i4();
-    public_vals[44] = AbstractInterval::one();
-    let refinment_target_indicies_pv: Vec<usize> = vec![40, 41];
-
-    // ############### Target Program ###################################
-    let program = add_program(4, 4);
-    let (true_abstract_states, true_abstract_traces) = run_ziren_program(&program);
-
-    let mut base_abs_main_trace_data = vec![];
-    for st in &true_abstract_traces {
-        if st.0 == "Cpu" {
-            base_abs_main_trace_data = st.1[..num_extracted_rows].to_vec();
-        }
-    }
-
-    let mut target_cols = (0..NUM_CPU_COLS).collect::<Vec<_>>();
-    target_cols.retain(|x| !program_cols.contains(x));
     for k in 1..(target_cols.len() + 1) {
         for combo in target_cols.iter().combinations(k) {
             let mut abs_main_trace_data = base_abs_main_trace_data.clone();
@@ -117,26 +79,7 @@ fn main() -> Result<(), ()> {
 
             if let Some(trace) = result {
                 println!("\nFind SAT assignment: {}", trace);
-
-                let recovered_states = trace
-                    .data
-                    .iter()
-                    .map(|row| ziren_abstract_trace_to_abstract_state(row, prime))
-                    .collect::<Vec<_>>();
-                for rs in &recovered_states {
-                    println!("{}", rs);
-                }
-                println!("========");
-
-                let program = add_program(
-                    recovered_states[0].pc.as_canonical_u32(prime),
-                    recovered_states[0].pc.as_canonical_u32(prime),
-                );
-                let (true_abstract_states, _true_abstract_traces) = run_ziren_program(&program);
-                for tas in &true_abstract_states {
-                    println!("{}", tas);
-                }
-
+                final_check(&trace, prime);
                 found_solution_flag = true;
                 break;
             }
@@ -146,6 +89,98 @@ fn main() -> Result<(), ()> {
             break;
         }
     }
+}
+
+fn main() -> Result<(), ()> {
+    // ############### Global Parameters #################################
+    let prime = 2_u32.pow(31) - 2_u32.pow(24) + 1;
+    let program_cols = (8..20).collect::<Vec<_>>();
+
+    // ############### Gather Constraints ################################
+    let air = CpuChip::default();
+    let symbolic_constraints: Vec<SymbolicExpression<Mersenne31>> =
+        get_symbolic_constraints(&air, 0, ZKM_PROOF_NUM_PV_ELTS);
+    let tv_constraints = symbolic_constraints
+        .iter()
+        .map(|sc| convert_p3_expr::<Mersenne31>(&sc))
+        .collect::<Vec<_>>();
+
+    // # Additional Public Value Verification
+    let (pv_pos_constraints, pv_neg_constraints) = get_pv_constraints();
+
+    // # Gather Symbolic Constraints
+    let constraints = LatticeVMConstraints {
+        air_constraints: tv_constraints.clone(),
+        pv_pos_constraints,
+        pv_neg_constraints,
+    };
+
+    // ############### Construct SMT formula ############################
+    //let smt = expr_to_smt(&constraints, 1, 68, prime);
+    //println!("{}", smt);
+
+    // ############### Preparation of Solver ############################
+    let mut rng = StdRng::seed_from_u64(42);
+    let max_row_id = 0;
+    let num_extracted_rows = 2;
+    let potential_boolean_vars = gather_boolean_variables(&tv_constraints);
+
+    // ############### Prepare Public Values ############################
+    let mut public_vals = vec![AbstractInterval::zero(); ZKM_PROOF_NUM_PV_ELTS];
+    public_vals[40] = AbstractInterval::i4();
+    public_vals[41] = AbstractInterval::i4();
+    public_vals[44] = AbstractInterval::one();
+    let refinment_target_indicies_pv: Vec<usize> = vec![40, 41];
+
+    // ############### Target Program ###################################
+    let program = add_program(4, 4);
+    let (true_abstract_states, true_abstract_traces) = run_ziren_program(&program);
+
+    let mut base_abs_main_trace_data = vec![];
+    for st in &true_abstract_traces {
+        if st.0 == "Cpu" {
+            base_abs_main_trace_data = st.1[..num_extracted_rows].to_vec();
+        }
+    }
+
+    // ############## Final Check Function ##############################
+    fn final_check(trace: &AbstractTrace, prime: u32) {
+        let recovered_states = trace
+            .data
+            .iter()
+            .map(|row| ziren_abstract_trace_to_abstract_state(row, prime))
+            .collect::<Vec<_>>();
+        for rs in &recovered_states {
+            println!("{}", rs);
+        }
+        println!("========");
+
+        let program = add_program(
+            recovered_states[0].pc.as_canonical_u32(prime),
+            recovered_states[0].pc.as_canonical_u32(prime),
+        );
+        let (true_abstract_states, _true_abstract_traces) = run_ziren_program(&program);
+        for tas in &true_abstract_states {
+            println!("{}", tas);
+        }
+    }
+
+    // ############## Solve! ###########################################
+    let mut target_cols = (0..NUM_CPU_COLS).collect::<Vec<_>>();
+    target_cols.retain(|x| !program_cols.contains(x));
+
+    run_solver(
+        &constraints,
+        &target_cols,
+        &potential_boolean_vars,
+        &refinment_target_indicies_pv,
+        &base_abs_main_trace_data,
+        public_vals,
+        max_row_id,
+        final_check,
+        prime,
+        42,
+    );
 
     Ok(())
 }
