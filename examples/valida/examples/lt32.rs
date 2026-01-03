743613765a5f71f661e6adadcbdd1507db36834f
Koukyosyumei <koukyosyumei@hotmail.com>
2026-01-03 14:32:35 +0900

    update

--- examples/valida/examples/lt32.rs
+++ examples/valida/examples/lt32.rs
@@ -1,49 +1,14 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
-use std::rc::Rc;
-use std::time;
-use std::{io, thread, time::Duration};
-
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use rand::rngs::StdRng;
-use rand::thread_rng;
-use rand::SeedableRng;
-use ratatui::{
-    backend::CrosstermBackend,
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Style},
-    widgets::{Block, Borders, Paragraph},
-    Terminal,
-};
+use std::io;
 
 use p3_baby_bear::BabyBear;
-use p3_challenger::DuplexChallenger;
-use p3_commit::ExtensionMmcs;
-use p3_dft::Radix2Bowers;
-use p3_field::extension::BinomialExtensionField;
-use p3_field::{AbstractField, Field, PrimeField32, TwoAdicField};
-use p3_fri::FriConfig;
-use p3_fri::{TwoAdicFriPcs, TwoAdicFriPcsConfig};
-use p3_keccak::Keccak256Hash;
-use p3_matrix::dense::RowMajorMatrix;
-use p3_matrix::Matrix;
-use p3_mds::coset_mds::CosetMds;
-use p3_merkle_tree::FieldMerkleTreeMmcs;
-use p3_poseidon::Poseidon;
-use p3_symmetric::{CompressionFunctionFromHasher, SerializingHasher32};
-//use p3_uni_stark::symbolic_builder::get_symbolic_constraints;
 
 use valida_alu_u32::add::columns::ADD_COL_MAP;
 use valida_alu_u32::add::Add32Chip;
-use valida_alu_u32::add::{columns::NUM_ADD_COLS, Add32Instruction, MachineWithAdd32Chip};
-use valida_alu_u32::bitwise::columns::COL_MAP;
-use valida_alu_u32::bitwise::Bitwise32Chip;
+use valida_alu_u32::add::{columns::NUM_ADD_COLS, Add32Instruction};
 use valida_alu_u32::com::columns::COM_COL_MAP;
+use valida_alu_u32::com::columns::NUM_COM_COLS;
 use valida_alu_u32::com::Com32Chip;
 use valida_alu_u32::com::Eq32Instruction;
 use valida_alu_u32::com::Ne32Instruction;
@@ -51,69 +16,25 @@ use valida_alu_u32::lt::columns::LT_COL_MAP;
 use valida_alu_u32::lt::columns::NUM_LT_COLS;
 use valida_alu_u32::lt::Lt32Chip;
 use valida_alu_u32::lt::Lt32Instruction;
-use valida_alu_u32::mul::Mul32Chip;
-use valida_alu_u32::sub::columns::NUM_SUB_COLS;
-use valida_alu_u32::sub::columns::SUB_COL_MAP;
-use valida_alu_u32::sub::Sub32Chip;
-use valida_alu_u32::sub::Sub32Instruction;
 use valida_basic_api::BasicMachine;
-use valida_basic_api::BasicMachineMetrics;
-use valida_basic_api::ValidaRuntime;
-use valida_cpu::BneInstruction;
 use valida_cpu::Imm32Instruction;
-use valida_cpu::MachineWithRegisters;
 use valida_cpu::StopInstruction;
-use valida_cpu::{
-    columns::{CPU_COL_MAP, NUM_CPU_COLS},
-    CpuChip,
-};
-use valida_machine::symbolic::symbolic_builder::{
-    get_lookup_interactions, get_symbolic_constraints, get_symbolic_lookups, SymbolicAirBuilder,
-};
-use valida_machine::symbolic::symbolic_expression::SymbolicExpression;
-use valida_machine::Chip;
-use valida_machine::ChipWithPersistence;
-use valida_machine::StarkConfig;
-use valida_machine::StarkConfigImpl;
-use valida_machine::{
-    check_constraints::display_interaction, Instruction, InstructionWord, Machine, MachineProof,
-    MachineRuntime, MemoryBackendTrait, MultiSegmentMachineProof, Operands, ProgramROM,
-    ProverOptions, SegmentMachine, StarkField, ValidaMemoryBackend, Word,
-};
+use valida_machine::{Instruction, InstructionWord, Operands, StarkField};
 use valida_opcodes::BYTES_PER_INSTR;
-use valida_program::MachineWithProgramROM;
-use valida_program::ProgramTableType;
 
-use latticevm::interval::AbstractInterval;
-use latticevm::interval::MayBeFlag;
-use latticevm::solver::run_solver;
-use latticevm::solver::RangeType;
-use latticevm::symbolic::eval_air_constraints;
-use latticevm::symbolic::eval_constraints;
-use latticevm::symbolic::gather_boolean_variables;
+use latticevm::quick::quick_api;
 use latticevm::symbolic::AbstractTrace;
 use latticevm::symbolic::LatticeVMConstraints;
+use latticevm::ui::generate_alu_final_checker;
 use latticevm::ui::UiState;
 use latticevm::utils::create_or_clear_dir;
 
-use latticevm_valida::alu_constraints::get_alu_constraint;
-use latticevm_valida::alu_tables::{derive_add_table, derive_com_table, derive_sub_table};
-use latticevm_valida::config::{get_machine_config, prover_options, MyConfig};
-use latticevm_valida::p3_to_tv::get_converted_symbolicconstraints;
-use latticevm_valida::state::valida_abstract_trace_to_abstract_state;
+use latticevm_valida::config::MyConfig;
+use latticevm_valida::utils::extract_constraints_and_range;
 use latticevm_valida::utils::{
-    generate_bootstrap_trace_from_program, make_pc_adjuster, refine_pc_interval,
+    dummy_adjust_pc_program, dummy_program_counter_refine_fn, dummy_table_deriver,
+    generate_bootstrap_trace_from_program,
 };
-
-fn program_counter_refine_fn(
-    abs_main_trace_data: &mut Vec<Vec<AbstractInterval>>,
-    program_len: usize,
-    i: usize,
-    j: usize,
-) {
-}
-
-fn adjust_pc_program(main_trace: &mut AbstractTrace, prime: u32) {}
 
 // ############## Final Check Function ##############################
 fn final_check(
@@ -133,10 +54,10 @@ fn final_check(
         trace.data[0][5],
         trace.data[0][6],
         trace.data[0][7],
-        trace.data[0][12],
-        trace.data[0][13],
-        trace.data[0][14],
-        trace.data[0][15],
+        trace.data[0][21],
+        0,
+        0,
+        0,
     );
 
     if !known_reprt.contains(&string_representation) {
@@ -184,6 +105,15 @@ fn main() -> Result<(), io::Error> {
     // ######################## Prime and Column Settings ########################
     let prime = 2_u32.pow(31) - 2_u32.pow(27) + 1;
 
+    // ######################## Solver Parameters ###############################
+    let max_iteration = 100000000;
+    let min_row_id = 0;
+    let max_row_id = 0;
+    let num_extracted_rows = 1;
+    let seed = 41;
+    let aux_tg_fns: Vec<_> = vec![dummy_table_deriver];
+    //let aux_tg_fns: Vec<_> = vec![dummy_table_deriver];
+
     // ######################## Extract Add Constraints ##########################
     println!("LT AIR MAP");
     println!("  {:?}", LT_COL_MAP);
@@ -192,102 +122,68 @@ fn main() -> Result<(), io::Error> {
     let num_col = NUM_LT_COLS;
     let chip_idx = 8;
 
-    //  input_1: Word([16, 17, 18, 19]), input_2: Word([20, 21, 22, 23])
-    //  output 37
     let machine = BasicMachine::<BabyBear>::default();
-    let (mut alu_constraint, cpu_input_cols) =
-        get_alu_constraint::<BasicMachine<BabyBear>, MyConfig, _>(&machine, &air, num_col, prime);
-    alu_constraint.aux_refinement_plan.push(cpu_input_cols[0]);
-    alu_constraint.aux_refinement_plan.push(cpu_input_cols[4]);
-    alu_constraint
-        .aux_range_types
-        .insert(cpu_input_cols[0], RangeType::U4);
-    alu_constraint
-        .aux_range_types
-        .insert(cpu_input_cols[4], RangeType::U4);
-    let minimum_num_taregt_cols = alu_constraint.aux_refinement_plan.len();
+    let (tv_constraints, mut refinable_cols, range_types, general_lookup_info) =
+        extract_constraints_and_range::<BasicMachine<BabyBear>, MyConfig, _>(
+            &machine, &air, num_col, prime,
+        );
+    let is_reals = [23, 24, 25, 26];
+    refinable_cols.retain(|e| !is_reals.contains(e));
+    refinable_cols.extend(&[21]);
 
-    println!("  Target Columns: {:?}", alu_constraint.aux_refinement_plan);
-    println!("  Range Types   : {:?}", alu_constraint.aux_range_types);
+    for t in &tv_constraints {
+        println!("#### {}", t);
+    }
+    println!("{:?}", refinable_cols);
+    println!("{:?}", range_types);
 
-    let aux_objs = vec![];
-    let aux_tg_fns = vec![derive_add_table, derive_sub_table, derive_com_table];
-
-    // ######################## Solver Parameters ###############################
-    let max_iteration = 10000000;
-    let min_row_id = 0;
-    let max_row_id = 0;
-    let seed = 41;
-
-    // Public trace values (example: program start, memory base, initial step)
-    let mut public_vals = vec![AbstractInterval::zero(); 3];
-    public_vals[0] = AbstractInterval::from_i64(0);
-    public_vals[1] = AbstractInterval::from_i64(4096);
-    public_vals[2] = AbstractInterval::from_i64(1);
-    let refinment_target_indicies_pv: Vec<usize> = vec![0, 1, 2];
+    let constraints = LatticeVMConstraints {
+        air_constraints: tv_constraints.clone(),
+        pv_pos_constraints: vec![],
+        pv_neg_constraints: vec![],
+    };
+    let minimum_num_taregt_cols = refinable_cols.len();
 
     // ######################## Program Initialization ###########################
     let program = get_target_program::<BabyBear>(3, 4);
-    let program_len = program.len();
-
-    // Convert program to string for UI display
     let program_str = program
         .iter()
         .map(|inst| format!("{}\n", inst))
         .collect::<String>();
 
-    // Generate initial abstract main trace from program
     let base_abs_main_trace_data =
         generate_bootstrap_trace_from_program(&program, chip_idx, 0, 0x1000);
+    for row in &base_abs_main_trace_data {
+        for v in row {
+            print!("{},", v);
+        }
+        println!("");
+    }
 
-    // ######################## UI Initialization ################################
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    let mut ui = UiState::new();
-    ui.program = program_str;
-
-    // ######################## Run Solver ######################################
-    let mut known_solution = HashSet::<String>::new();
-    let mut logs = Vec::new();
-    let start_time = time::Instant::now();
-    run_solver(
-        &alu_constraint.aux_constraints,
-        &alu_constraint.aux_refinement_plan,
-        &alu_constraint.aux_range_types,
-        &aux_objs,
+    // ######################## Solve ############################################
+    quick_api(
+        program_str,
+        &constraints,
+        &refinable_cols,
+        &range_types,
+        &vec![],
         &aux_tg_fns,
-        &refinment_target_indicies_pv,
+        &vec![],
         &base_abs_main_trace_data,
-        public_vals,
+        vec![],
         max_iteration,
         minimum_num_taregt_cols,
         min_row_id,
         max_row_id,
-        program_len,
-        program_counter_refine_fn,
-        adjust_pc_program,
+        program.len(),
+        dummy_program_counter_refine_fn,
+        dummy_adjust_pc_program,
         final_check,
         prime,
         seed,
-        &mut known_solution,
-        &mut logs,
-        &mut ui,
-        &mut terminal,
-    );
-
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
-    eprintln!("Execution Time    : {:?}", start_time.elapsed());
-    eprintln!("#Unique Solution  : {}", known_solution.len());
-
-    Ok(())
+    )
 }
+
+/*
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 39, 40, 41, 42, 43, 44, 37
+*/
