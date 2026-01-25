96b5af67ae87ea5ebc9a43e78c10159950d5ee41
Koukyosyumei <koukyosyumei@hotmail.com>
2026-01-25 10:07:38 -0500

    update

---
+++ examples/pico/examples/lessthan.rs
@@ -0,0 +1,100 @@
+use core::mem::transmute;
+use itertools::Itertools;
+use std::collections::HashSet;
+use std::fs;
+use std::io;
+
+use p3_koala_bear::KoalaBear;
+
+use pico_vm::chips::chips::alu::lt::LtChip;
+use pico_vm::chips::chips::alu::lt::{LtCols, NUM_LT_COLS};
+use pico_vm::compiler::riscv::program::Program;
+use pico_vm::compiler::riscv::{instruction::Instruction, opcode::Opcode, register::Register};
+
+use latticevm::quick::quick_api;
+use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn, RangeType};
+use latticevm::ui::{generate_alu_final_checker, UiState};
+use latticevm::utils::create_or_clear_dir;
+use latticevm::{symbolic::AbstractTrace, symbolic::LatticeVMConstraints};
+
+use latticevm_pico::lookup::get_symbolic_lookup_constraints;
+use latticevm_pico::utils::{
+    extract_constraints_and_range, generate_abstract_trace, get_program_str, indices_arr,
+};
+
+const fn make_col_map() -> LtCols<usize> {
+    let indices_arr = indices_arr::<{ NUM_LT_COLS }>();
+    unsafe { transmute::<[usize; NUM_LT_COLS], LtCols<usize>>(indices_arr) }
+}
+
+pub fn target_program(opcode: Opcode, pc_start: u32, pc_base: u32, x: u32, y: u32) -> Program {
+    let instructions = vec![Instruction::new(opcode, 1, x, y, true, true)];
+    Program::new(instructions, pc_start, pc_base)
+}
+
+pub fn get_opcode_addsub(target_opcode: &str) -> Opcode {
+    match target_opcode {
+        "SLT" => Opcode::SLT,
+        "SLTU" => Opcode::SLTU,
+        _ => panic!("unsupported instruction"),
+    }
+}
+
+fn main() -> Result<(), io::Error> {
+    let target_opcode = "SLT";
+
+    create_or_clear_dir("voutput")?;
+
+    // ######################## Prime and Column Settings ########################
+    let prime = 2_u32.pow(31) - 2_u32.pow(24) + 1;
+
+    // ######################## Solver Parameters ###############################
+    let max_iteration = 100000;
+    let min_row_id = 0;
+    let max_row_id = 0;
+    let num_extracted_rows = 1;
+    let seed = 41;
+
+    // ######################## Extract CPU Constraints ##########################
+    let air: LtChip<KoalaBear> = LtChip::default();
+    let air_name = "LessThan";
+    let _colmap = make_col_map();
+
+    let (tv_constraints, mut refinable_cols, mut range_types, general_lookup_info) =
+        extract_constraints_and_range::<KoalaBear, LtChip<KoalaBear>>(&air, NUM_LT_COLS, prime);
+    let final_check = generate_alu_final_checker(general_lookup_info.clone());
+    refinable_cols.extend(&general_lookup_info.alu_output);
+
+    let constraints = LatticeVMConstraints {
+        air_constraints: tv_constraints.clone(),
+        pv_pos_constraints: vec![],
+        pv_neg_constraints: vec![],
+    };
+    let minimum_num_taregt_cols = 1; //refinable_cols.len();
+
+    // ######################## Program Initialization ###########################
+    let program = target_program(get_opcode_addsub(&target_opcode), 4, 4, 2, 3);
+    let base_abs_main_trace_data =
+        generate_abstract_trace(&program, air_name.to_string(), num_extracted_rows);
+
+    // ######################## Solve ############################################
+    quick_api(
+        get_program_str(&program),
+        &constraints,
+        &refinable_cols,
+        &range_types,
+        &vec![],
+        &base_abs_main_trace_data,
+        vec![],
+        max_iteration,
+        minimum_num_taregt_cols,
+        min_row_id,
+        max_row_id,
+        program.instructions.len(),
+        dummy_program_counter_refine_fn,
+        dummy_adjust_pc_program,
+        final_check,
+        prime,
+        seed,
+    )
+}
