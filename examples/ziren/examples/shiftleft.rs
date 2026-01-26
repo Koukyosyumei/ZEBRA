78fad81fd9b3c7b8f501a6fc97df2d1abc245be2
Koukyosyumei <koukyosyumei@hotmail.com>
2026-01-26 06:06:26 -0500

    update

--- examples/ziren/examples/shiftleft.rs
+++ examples/ziren/examples/shiftleft.rs
@@ -1,75 +1,21 @@
-use core::mem::{size_of, transmute};
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::fs;
-use std::rc::Rc;
-use std::time;
-use std::{io, thread, time::Duration};
+use core::mem::transmute;
+use std::io;
 
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use itertools::Itertools;
-use rand::{rngs::StdRng, SeedableRng};
-use ratatui::{
-    backend::CrosstermBackend,
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Style},
-    widgets::{Block, Borders, Paragraph},
-    Terminal,
-};
 
-use p3_air::Air;
-use p3_air::PairCol;
-use p3_field::Field;
 use p3_koala_bear::KoalaBear;
-use p3_matrix::dense::RowMajorMatrix;
-use p3_mersenne_31::Mersenne31;
-use p3_uni_stark::SymbolicAirBuilder;
-use p3_uni_stark::{get_symbolic_constraints, SymbolicExpression};
 
-use zkm_core_executor::syscalls::SyscallCode;
-use zkm_core_executor::ExecutionRecord;
-use zkm_core_executor::Executor;
-use zkm_core_executor::MipsAirId::MemoryLocal;
 use zkm_core_executor::{Instruction, Opcode, Program};
-use zkm_core_machine::alu::LtCols;
 use zkm_core_machine::alu::ShiftLeftCols;
-use zkm_core_machine::alu::NUM_ADD_SUB_COLS;
-use zkm_core_machine::alu::NUM_LT_COLS;
 use zkm_core_machine::alu::NUM_SHIFT_LEFT_COLS;
-use zkm_core_machine::memory::MemoryLocalChip;
-use zkm_core_machine::AddSubChip;
-use zkm_core_machine::LtChip;
-use zkm_core_machine::MulChip;
 use zkm_core_machine::ShiftLeft;
-use zkm_core_machine::{
-    cpu::columns::{CPU_COL_MAP, NUM_CPU_COLS},
-    CpuChip,
-};
-use zkm_stark::LookupBuilder;
-use zkm_stark::LookupKind;
-use zkm_stark::MachineAir;
 use zkm_stark::MachineProver;
-use zkm_stark::ZKMCoreOpts;
-use zkm_stark::ZKM_PROOF_NUM_PV_ELTS;
 
 use latticevm::quick::quick_api;
-use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn, RangeType};
+use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn};
 use latticevm::ui::generate_alu_final_checker;
-use latticevm::ui::save_repr_if_unique;
-use latticevm::ui::UiState;
-use latticevm::utils::trace_fmt_with_idxs;
 use latticevm::utils::{create_or_clear_dir, indices_arr};
-use latticevm::{symbolic::AbstractTrace, symbolic::LatticeVMConstraints};
+use latticevm::symbolic::LatticeVMConstraints;
 
-use latticevm_ziren::executor::run_ziren_program;
-use latticevm_ziren::lookup::get_symbolic_lookup_constraints;
-use latticevm_ziren::p3_to_tv::{convert_p3_expr, convert_p3_virtual_pair_col};
-use latticevm_ziren::pv_constraints::get_pv_constraints;
-use latticevm_ziren::state::ziren_abstract_trace_to_abstract_state;
 use latticevm_ziren::utils::{
     extract_constraints_and_range, generate_abstract_trace, get_program_str,
 };
@@ -80,7 +26,7 @@ const fn make_col_map() -> ShiftLeftCols<usize> {
 }
 
 pub fn target_program(pc_start: u32, pc_base: u32) -> Program {
-    let mut instructions = vec![Instruction::new(Opcode::SLL, 1, 2, 3, true, true)];
+    let instructions = vec![Instruction::new(Opcode::SLL, 1, 2, 3, true, true)];
     Program::new(instructions, pc_start, pc_base)
 }
 
@@ -102,7 +48,7 @@ fn main() -> Result<(), io::Error> {
     let air_name = "ShiftLeft";
     let _colmap = make_col_map();
 
-    let (tv_constraints, mut refinable_cols, mut range_types, general_lookup_info) =
+    let (tv_constraints, mut refinable_cols, range_types, general_lookup_info) =
         extract_constraints_and_range::<KoalaBear, ShiftLeft>(&air, NUM_SHIFT_LEFT_COLS, prime);
     let final_check = generate_alu_final_checker(general_lookup_info.clone());
     refinable_cols.extend(&general_lookup_info.alu_output);
