be09d4884fa11de3838928a033421f1c9a5fe686
Koukyosyumei <koukyosyumei@hotmail.com>
2026-01-28 15:05:52 -0500

    update

--- examples/ziren/src/utils.rs
+++ examples/ziren/src/utils.rs
@@ -12,7 +12,7 @@ use zkm_stark::LookupBuilder;
 use zkm_stark::MachineProver;
 use zkm_stark::ZKM_PROOF_NUM_PV_ELTS;
 
-use latticevm::solver::RangeType;
+use latticevm::solver::{prepare_constraints_and_range_type, RangeType};
 use latticevm::symbolic::gather_vars;
 use latticevm::symbolic::LatticeVMSymbolicExpr;
 use latticevm::symbolic::{is_iszero_operator, is_koalabear_word_range};
@@ -79,61 +79,20 @@ where
         prime,
     );
 
-    let mut refinable_cols: Vec<usize> = (0..num_cols).collect();
-    refinable_cols.retain(|c| !multiplicities.contains(c));
-    refinable_cols.retain(|c| !received_vars_from_cpu.contains(c));
-
     let mut tv_constraints = symbolic_constraints
         .iter()
         .map(|sc| convert_p3_expr::<F>(&sc))
         .collect::<Vec<_>>();
 
-    let mut new_tv_constraints = Vec::new();
-    let mut is_in_koalabear_word_range_check = false;
-    let mut is_in_iszero_operator = false;
-    for t in &tv_constraints {
-        if let Some(exprs) = is_iszero_operator(t, prime) {
-            if is_in_iszero_operator {
-                is_in_iszero_operator = false;
-            } else {
-                new_tv_constraints.push(exprs[0].clone());
-                new_tv_constraints.push(exprs[1].clone());
-                is_in_iszero_operator = true;
-            }
-        } else {
-            if let Some(expr) = is_koalabear_word_range(t, prime) {
-                if is_in_koalabear_word_range_check {
-                    is_in_koalabear_word_range_check = false;
-                } else {
-                    new_tv_constraints.push(expr);
-                    is_in_koalabear_word_range_check = true;
-                }
-            } else {
-                if (!is_in_koalabear_word_range_check) && (!is_in_iszero_operator) {
-                    new_tv_constraints.push(t.clone());
-                }
-            }
-        }
-    }
-    tv_constraints = new_tv_constraints;
-
-    tv_constraints.extend(lookup_symbolic_constraints);
-
-    let mut used_vars = HashSet::new();
-    for t in &tv_constraints {
-        gather_vars(0, t, &mut used_vars);
-    }
-    let used_var_ids: HashSet<usize> = used_vars.iter().map(|x| x.1).collect();
-    refinable_cols.retain(|c| used_var_ids.contains(c));
-
-    let potential_boolean_vars = gather_boolean_variables(&tv_constraints, &multiplicities);
-    let mut range_types: HashMap<usize, RangeType> = potential_boolean_vars
-        .iter()
-        .map(|k| (*k, RangeType::Bool))
-        .collect();
-    for c in &u8_cols {
-        range_types.insert(*c, RangeType::U8);
-    }
+    let (refinable_cols, range_types) = prepare_constraints_and_range_type(
+        num_cols,
+        &u8_cols,
+        &multiplicities,
+        &received_vars_from_cpu,
+        &mut tv_constraints,
+        lookup_symbolic_constraints,
+        prime,
+    );
 
     (
         tv_constraints,
