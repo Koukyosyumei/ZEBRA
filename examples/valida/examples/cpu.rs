335f60621c3186dec92e1b73a212da42b5e6920e
Koukyosyumei <koukyosyumei@hotmail.com>
2026-01-30 15:32:44 -0500

    update

--- examples/valida/examples/cpu.rs
+++ examples/valida/examples/cpu.rs
@@ -23,15 +23,68 @@ use latticevm::quick::quick_api;
 use latticevm::solver::{
     dummy_adjust_pc_program, dummy_program_counter_refine_fn, dummy_table_deriver,
 };
+use latticevm::state::MemoryOp;
+use latticevm::state::MemoryOpKind;
 use latticevm::symbolic::{AbstractTrace, LatticeVMConstraints};
 use latticevm::ui::UiState;
 use latticevm::utils::create_or_clear_dir;
 
 use latticevm_valida::config::MyConfig;
-use latticevm_valida::state::valida_abstract_trace_to_abstract_state;
 use latticevm_valida::utils::{
     extract_constraints_and_range, generate_bootstrap_trace_from_program, make_pc_adjuster,
 };
+
+fn reconstruct_word(row: &[AbstractInterval], base: usize) -> AbstractInterval {
+    let mut val = AbstractInterval::from_i64(0);
+    let mut mul = 1_i64;
+    for i in 0..4 {
+        val = val + row[base + i].clone() * AbstractInterval::from_i64(mul);
+        mul *= 256;
+    }
+    val
+}
+
+pub fn valida_abstract_trace_to_abstract_state(
+    abstract_row: &Vec<AbstractInterval>,
+    prime: u32,
+) -> AbstractState {
+    let mut memory_ops = Vec::new();
+
+    if abstract_row[30].is_non_zero(prime) != MayBeFlag::False {
+        memory_ops.push(MemoryOp {
+            kind: MemoryOpKind::Read,
+            addr: abstract_row[31].clone(),
+            value: reconstruct_word(abstract_row, 32),
+        });
+    }
+
+    if abstract_row[36].is_non_zero(prime) != MayBeFlag::False {
+        memory_ops.push(MemoryOp {
+            kind: MemoryOpKind::Read,
+            addr: abstract_row[37].clone(),
+            value: reconstruct_word(abstract_row, 38),
+        });
+    }
+
+    if abstract_row[42].is_non_zero(prime) != MayBeFlag::False {
+        memory_ops.push(MemoryOp {
+            kind: MemoryOpKind::Write,
+            addr: abstract_row[43].clone(),
+            value: reconstruct_word(abstract_row, 44),
+        });
+    }
+
+    AbstractState {
+        clk: abstract_row[0].clone(),
+        pc: abstract_row[1].clone(),
+        is_done: match abstract_row[24].clone().is_zero(prime) {
+            MayBeFlag::True => MayBeFlag::False,
+            MayBeFlag::False => MayBeFlag::True,
+            MayBeFlag::MayBe => MayBeFlag::MayBe,
+        },
+        memory_ops,
+    }
+}
 
 // ############## Final Check Function ##############################
 fn final_check(
--- examples/valida/src/lib.rs
+++ examples/valida/src/lib.rs
@@ -1,5 +1,3 @@
 pub mod config;
-pub mod lookup;
 pub mod p3_to_tv;
-pub mod state;
 pub mod utils;
