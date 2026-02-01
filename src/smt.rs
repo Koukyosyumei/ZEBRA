1ca1e1a0c0619f5f86d0eb58d624ae30a105eb78
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-01 16:28:47 -0500

    update

--- src/smt.rs
+++ src/smt.rs
@@ -5,160 +5,6 @@ use crate::solver::RangeType;
 use crate::symbolic::LatticeVMConstraints;
 use crate::symbolic::{LatticeVMSymbolicEntry, LatticeVMSymbolicExpr};
 
-/// Converts the given expression into SMT-LIB constraints over all rows.
-pub fn expr_to_smt(
-    constraints: &LatticeVMConstraints,
-    constants: &Vec<(usize, usize, AbstractInterval)>,
-    neg_constants: &Vec<(usize, usize, AbstractInterval)>,
-    n_rows: usize,
-    n_cols: usize,
-    n_pvs: usize,
-    prime: u32,
-) -> String {
-    fn helper(
-        expr: &LatticeVMSymbolicExpr,
-        row_id: usize,
-        n_rows: usize,
-        n_pvs: usize,
-        vars: &mut HashSet<String>,
-    ) -> String {
-        match expr {
-            LatticeVMSymbolicExpr::IsFirstRow => {
-                if row_id == 0 {
-                    "1".to_string()
-                } else {
-                    "0".to_string()
-                }
-            }
-            LatticeVMSymbolicExpr::IsTransition => {
-                if row_id < n_rows - 1 {
-                    "1".to_string()
-                } else {
-                    "0".to_string()
-                }
-            }
-            LatticeVMSymbolicExpr::IsLastRow => {
-                if row_id == n_rows - 1 {
-                    "1".to_string()
-                } else {
-                    "0".to_string()
-                }
-            }
-            LatticeVMSymbolicExpr::Constant(AbstractInterval { lo, hi: _hi }) => {
-                format!("{}", lo)
-            }
-            LatticeVMSymbolicExpr::Variable(v) => {
-                // "curr" -> a_row_index, "next" -> a_row+1_index
-                let (ty, base_row) = match v.entry {
-                    LatticeVMSymbolicEntry::Main { is_curr } => {
-                        if is_curr {
-                            ("trace", row_id)
-                        } else {
-                            ("trace", row_id + 1)
-                        }
-                    }
-                    LatticeVMSymbolicEntry::Public => ("public", 0),
-                    _ => todo!(),
-                };
-                let name = format!("{}_{}_{}", ty, base_row, v.index);
-                vars.insert(name.clone());
-                name
-            }
-            LatticeVMSymbolicExpr::Add(a, b) => {
-                format!(
-                    "(+ {} {})",
-                    helper(a, row_id, n_rows, n_pvs, vars),
-                    helper(b, row_id, n_rows, n_pvs, vars)
-                )
-            }
-            LatticeVMSymbolicExpr::Sub(a, b) => {
-                format!(
-                    "(- {} {})",
-                    helper(a, row_id, n_rows, n_pvs, vars),
-                    helper(b, row_id, n_rows, n_pvs, vars)
-                )
-            }
-            LatticeVMSymbolicExpr::Mul(a, b) => {
-                format!(
-                    "(* {} {})",
-                    helper(a, row_id, n_rows, n_pvs, vars),
-                    helper(b, row_id, n_rows, n_pvs, vars)
-                )
-            }
-            LatticeVMSymbolicExpr::Neg(a) => {
-                format!("(- {})", helper(a, row_id, n_rows, n_pvs, vars))
-            }
-            _ => {
-                todo!()
-            }
-        }
-    }
-
-    let mut vars = HashSet::new();
-    let mut smt = String::new();
-    smt.push_str("(set-logic QF_NIA)\n");
-    // smt.push_str("(set-option :incremental true)\n");
-    // smt.push_str(&format!("(define-sort F () (_ FiniteField {}))\n", prime));
-
-    // Declare all trace variables
-    for i in 0..(n_rows + 1) {
-        for j in 0..n_cols {
-            let name = format!("trace_{}_{}", i, j);
-            vars.insert(name.clone());
-        }
-    }
-    for i in 0..n_pvs {
-        let name = format!("public_0_{}", i);
-        vars.insert(name.clone());
-    }
-    for v in &vars {
-        smt.push_str(&format!("(declare-fun {} () Int)\n", v));
-    }
-
-    // Add modular constraints for each row
-    for expr in &constraints.air_constraints {
-        for i in 0..n_rows {
-            let body = helper(expr, i, n_rows, n_pvs, &mut vars);
-            smt.push_str(&format!("(assert (= (mod {} {}) 0))\n", body, prime));
-        }
-    }
-
-    for expr in &constraints.pv_pos_constraints {
-        for i in 0..n_rows {
-            let body = helper(expr, i, n_rows, n_pvs, &mut vars);
-            smt.push_str(&format!("(assert (= (mod {} {}) 0))\n", body, prime));
-        }
-    }
-
-    for expr in &constraints.pv_neg_constraints {
-        for i in 0..n_rows {
-            let body = helper(expr, i, n_rows, n_pvs, &mut vars);
-            smt.push_str(&format!("(assert (not (= (mod {} {}) 0)))\n", body, prime));
-        }
-    }
-
-    for (i, j, v) in constants {
-        smt.push_str(&format!(
-            "(assert (= trace_{}_{} {}))\n",
-            i,
-            j,
-            v.as_canonical_u32(prime)
-        ));
-    }
-
-    for (i, j, v) in neg_constants {
-        smt.push_str(&format!(
-            "(assert (not (= trace_{}_{} {})))\n",
-            i,
-            j,
-            v.as_canonical_u32(prime)
-        ));
-    }
-
-    smt.push_str("(check-sat)\n(get-model)\n");
-    smt
-}
-
 /// Converts the given expression into SMT-LIB constraints over all rows using BitVectors.
 pub fn expr_to_smt_bv(
     constraints: &LatticeVMConstraints,
@@ -243,7 +89,61 @@ pub fn expr_to_smt_bv(
                 // Two's complement negation
                 format!("(bvneg {})", helper(a, row_id, n_rows, n_pvs, vars))
             }
-            _ => todo!(),
+            LatticeVMSymbolicExpr::WhenNonZero(a, b) => {
+                format!(
+                    "(ite (= {} #x00000000) #x00000000 {})",
+                    helper(a, row_id, n_rows, n_pvs, vars),
+                    helper(b, row_id, n_rows, n_pvs, vars)
+                )
+            }
+            LatticeVMSymbolicExpr::WhenZero(a, b) => {
+                format!(
+                    "(ite (= {} #x00000000) {} #x00000000)",
+                    helper(a, row_id, n_rows, n_pvs, vars),
+                    helper(b, row_id, n_rows, n_pvs, vars)
+                )
+            }
+            LatticeVMSymbolicExpr::Lt(a, b) => {
+                format!(
+                    "(ite (bvult {} {}) #x00000000 #x00000001)",
+                    helper(a, row_id, n_rows, n_pvs, vars),
+                    helper(b, row_id, n_rows, n_pvs, vars),
+                )
+            }
+            LatticeVMSymbolicExpr::Flip(a) => {
+                format!(
+                    "(ite (= {} #x00000000) #x00000001 #x00000000)",
+                    helper(a, row_id, n_rows, n_pvs, vars),
+                )
+            }
+            LatticeVMSymbolicExpr::WordSrl(a_vec, b_vec) => {
+                let mut a_val = helper(&a_vec[0], row_id, n_rows, n_pvs, vars);
+                let mut factor = 1;
+                for w in a_vec.iter().skip(1) {
+                    factor *= 256;
+                    a_val = format!(
+                        "(bvadd {} (bvmul {} #x{:08x}))",
+                        a_val,
+                        helper(w, row_id, n_rows, n_pvs, vars),
+                        factor
+                    );
+                }
+
+                let mut b_val = helper(&b_vec[0], row_id, n_rows, n_pvs, vars);
+                let mut factor = 1;
+                for w in b_vec.iter().skip(1) {
+                    factor *= 256;
+                    b_val = format!(
+                        "(bvadd {} (bvmul {} #x{:08x}))",
+                        b_val,
+                        helper(w, row_id, n_rows, n_pvs, vars),
+                        factor
+                    );
+                }
+
+                format!("(bvlshr {} {})", a_val, b_val)
+            }
+            other => todo!("{:?} is not implemented", other),
         }
     }
 
@@ -276,6 +176,13 @@ pub fn expr_to_smt_bv(
                 "(assert (= (bvurem {} {}) #x00000000))\n",
                 body, prime_hex
             ));
+        }
+    }
+
+    for expr in &constraints.lookup_constraints {
+        for i in 0..n_rows {
+            let body = helper(expr, i, n_rows, n_pvs, &mut vars);
+            smt.push_str(&format!("(assert (= {} #x00000000))\n", body));
         }
     }
 
