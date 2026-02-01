5b274a84c7a442e0d5ede11da0db8311b768c141
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-01 15:12:09 -0500

    update

--- src/smt.rs
+++ src/smt.rs
@@ -8,6 +8,7 @@ use crate::symbolic::{LatticeVMSymbolicEntry, LatticeVMSymbolicExpr};
 pub fn expr_to_smt(
     constraints: &LatticeVMConstraints,
     constants: &Vec<(usize, usize, AbstractInterval)>,
+    neg_constants: &Vec<(usize, usize, AbstractInterval)>,
     n_rows: usize,
     n_cols: usize,
     n_pvs: usize,
@@ -138,6 +139,177 @@ pub fn expr_to_smt(
     for (i, j, v) in constants {
         smt.push_str(&format!(
             "(assert (= trace_{}_{} {}))\n",
+            i,
+            j,
+            v.as_canonical_u32(prime)
+        ));
+    }
+
+    for (i, j, v) in neg_constants {
+        smt.push_str(&format!(
+            "(assert (not (= trace_{}_{} {})))\n",
+            i,
+            j,
+            v.as_canonical_u32(prime)
+        ));
+    }
+
+    smt.push_str("(check-sat)\n(get-model)\n");
+    smt
+}
+
+/// Converts the given expression into SMT-LIB constraints over all rows using BitVectors.
+pub fn expr_to_smt_bv(
+    constraints: &LatticeVMConstraints,
+    constants: &Vec<(usize, usize, AbstractInterval)>,
+    neg_constants: &Vec<(usize, usize, AbstractInterval)>,
+    n_rows: usize,
+    n_cols: usize,
+    n_pvs: usize,
+    prime: u32, // 32-bit prime
+) -> String {
+    fn helper(
+        expr: &LatticeVMSymbolicExpr,
+        row_id: usize,
+        n_rows: usize,
+        n_pvs: usize,
+        vars: &mut HashSet<String>,
+    ) -> String {
+        match expr {
+            LatticeVMSymbolicExpr::IsFirstRow => {
+                if row_id == 0 {
+                    "#x1".to_string()
+                } else {
+                    "#x0".to_string()
+                }
+            }
+            LatticeVMSymbolicExpr::IsTransition => {
+                if row_id < n_rows - 1 {
+                    "#x1".to_string()
+                } else {
+                    "#x0".to_string()
+                }
+            }
+            LatticeVMSymbolicExpr::IsLastRow => {
+                if row_id == n_rows - 1 {
+                    "#x1".to_string()
+                } else {
+                    "#x0".to_string()
+                }
+            }
+            LatticeVMSymbolicExpr::Constant(AbstractInterval { lo, hi: _ }) => {
+                format!("#x{:08x}", lo)
+            }
+            LatticeVMSymbolicExpr::Variable(v) => {
+                let (ty, base_row) = match v.entry {
+                    LatticeVMSymbolicEntry::Main { is_curr } => {
+                        if is_curr {
+                            ("trace", row_id)
+                        } else {
+                            ("trace", row_id + 1)
+                        }
+                    }
+                    LatticeVMSymbolicEntry::Public => ("public", 0),
+                    _ => todo!(),
+                };
+                let name = format!("{}_{}_{}", ty, base_row, v.index);
+                vars.insert(name.clone());
+                name
+            }
+            LatticeVMSymbolicExpr::Add(a, b) => {
+                format!(
+                    "(bvadd {} {})",
+                    helper(a, row_id, n_rows, n_pvs, vars),
+                    helper(b, row_id, n_rows, n_pvs, vars)
+                )
+            }
+            LatticeVMSymbolicExpr::Sub(a, b) => {
+                format!(
+                    "(bvsub {} {})",
+                    helper(a, row_id, n_rows, n_pvs, vars),
+                    helper(b, row_id, n_rows, n_pvs, vars)
+                )
+            }
+            LatticeVMSymbolicExpr::Mul(a, b) => {
+                format!(
+                    "(bvmul {} {})",
+                    helper(a, row_id, n_rows, n_pvs, vars),
+                    helper(b, row_id, n_rows, n_pvs, vars)
+                )
+            }
+            LatticeVMSymbolicExpr::Neg(a) => {
+                // Two's complement negation
+                format!("(bvneg {})", helper(a, row_id, n_rows, n_pvs, vars))
+            }
+            _ => todo!(),
+        }
+    }
+
+    let mut vars = HashSet::new();
+    let mut smt = String::new();
+    smt.push_str("(set-logic QF_BV)\n");
+
+    // Declare all trace variables as 32-bit bitvectors
+    for i in 0..(n_rows + 1) {
+        for j in 0..n_cols {
+            let name = format!("trace_{}_{}", i, j);
+            vars.insert(name.clone());
+        }
+    }
+    for i in 0..n_pvs {
+        let name = format!("public_0_{}", i);
+        vars.insert(name.clone());
+    }
+    for v in &vars {
+        smt.push_str(&format!("(declare-fun {} () (_ BitVec 32))\n", v));
+    }
+
+    let prime_hex = format!("#x{:08x}", prime);
+
+    // Add modular constraints using bvurem
+    for expr in &constraints.air_constraints {
+        for i in 0..n_rows {
+            let body = helper(expr, i, n_rows, n_pvs, &mut vars);
+            smt.push_str(&format!(
+                "(assert (= (bvurem {} {}) #x00000000))\n",
+                body, prime_hex
+            ));
+        }
+    }
+
+    for expr in &constraints.pv_pos_constraints {
+        for i in 0..n_rows {
+            let body = helper(expr, i, n_rows, n_pvs, &mut vars);
+            smt.push_str(&format!(
+                "(assert (= (bvurem {} {}) #x00000000))\n",
+                body, prime_hex
+            ));
+        }
+    }
+
+    for expr in &constraints.pv_neg_constraints {
+        for i in 0..n_rows {
+            let body = helper(expr, i, n_rows, n_pvs, &mut vars);
+            smt.push_str(&format!(
+                "(assert (not (= (bvurem {} {}) #x00000000)))\n",
+                body, prime_hex
+            ));
+        }
+    }
+
+    // Constants
+    for (i, j, v) in constants {
+        smt.push_str(&format!(
+            "(assert (= trace_{}_{} #x{:08x}))\n",
+            i,
+            j,
+            v.as_canonical_u32(prime)
+        ));
+    }
+
+    for (i, j, v) in neg_constants {
+        smt.push_str(&format!(
+            "(assert (not (= trace_{}_{} #x{:08x})))\n",
             i,
             j,
             v.as_canonical_u32(prime)
