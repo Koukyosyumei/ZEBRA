99689cbbb681e1f65dc494a47edbab296a40d5c5
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-07 18:29:06 -0500

    update

--- examples/ziren/examples/lt.rs
+++ examples/ziren/examples/lt.rs
@@ -10,6 +10,7 @@ use zkm_core_machine::LtChip;
 use zkm_stark::MachineProver;
 
 use latticevm::quick::quick_api;
+use latticevm::quick::{experiment_harness, ProgramInfo, SearchConfig};
 use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn};
 use latticevm::symbolic::LatticeVMConstraints;
 use latticevm::ui::generate_alu_final_checker;
@@ -46,53 +47,42 @@ fn main() -> Result<(), io::Error> {
     // ######################## Prime and Column Settings ########################
     let prime = 2_u32.pow(31) - 2_u32.pow(24) + 1;
 
-    // ######################## Solver Parameters ###############################
-    let max_iteration = 10000000000;
-    let min_row_id = 0;
-    let max_row_id = 0;
-    let num_extracted_rows = 1;
-    let seed = 41;
-
     // ######################## Extract CPU Constraints ##########################
     let air = LtChip::default();
     let air_name = "Lt";
     let _colmap = make_col_map();
 
-    let (air_constraints, lookup_constraints, refinable_cols, range_types, general_lookup_info) =
+    let (mut constraint_info, general_lookup_info) =
         extract_constraints_and_range::<KoalaBear, LtChip>(&air, NUM_LT_COLS, prime);
     let final_check = generate_alu_final_checker(general_lookup_info.clone());
 
-    let constraints = LatticeVMConstraints {
-        air_constraints,
-        lookup_constraints,
-        pv_pos_constraints: vec![],
-        pv_neg_constraints: vec![],
-    };
-    let minimum_num_taregt_cols = 3; //refinable_cols.len();
+    constraint_info
+        .refinable_cols
+        .extend(&general_lookup_info.alu_output.clone());
+    constraint_info.output_columns = general_lookup_info.alu_output.clone();
 
     // ######################## Program Initialization ###########################
     let program = target_program(get_opcode(opcode_str), 4, 4, 2, 3);
-    let base_abs_main_trace_data =
-        generate_abstract_trace(&program, air_name.to_string(), num_extracted_rows);
+
+    // ######################## Set Info ##########################################
+    let program_info = ProgramInfo {
+        program_str: get_program_str(&program),
+        program_len: program.instructions.len(),
+    };
+    let base_abs_main_trace_data = generate_abstract_trace(&program, air_name.to_string(), 1);
+    let mut search_config = SearchConfig::new(constraint_info.refinable_cols.len());
+    search_config.minimum_num_taregt_cols = 4;
 
     // ######################## Solve ############################################
-    quick_api(
-        get_program_str(&program),
-        &constraints,
-        &refinable_cols,
-        &range_types,
-        &vec![],
+    experiment_harness(
+        &program_info,
+        &mut constraint_info,
+        &search_config,
         &base_abs_main_trace_data,
         vec![],
-        max_iteration,
-        minimum_num_taregt_cols,
-        min_row_id,
-        max_row_id,
-        program.instructions.len(),
+        &vec![], // vec![0],
         dummy_program_counter_refine_fn,
         dummy_adjust_pc_program,
         final_check,
-        prime,
-        seed,
     )
 }
