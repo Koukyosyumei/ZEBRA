049541834f1c7078dbdaa53d17c7333945358660
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-07 17:32:50 -0500

    update

--- examples/ziren/examples/addsub.rs
+++ examples/ziren/examples/addsub.rs
@@ -9,10 +9,10 @@ use zkm_core_machine::alu::{AddSubCols, NUM_ADD_SUB_COLS};
 use zkm_core_machine::AddSubChip;
 use zkm_stark::MachineProver;
 
+use latticevm::quick::experiment_harness;
 use latticevm::quick::ConstraintInfo;
 use latticevm::quick::ProgramInfo;
 use latticevm::quick::SearchConfig;
-use latticevm::quick::experiment_harness;
 use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn};
 use latticevm::ui::{save_repr_if_unique, UiState};
 use latticevm::utils::{create_or_clear_dir, indices_arr, trace_fmt_with_idxs};
@@ -84,32 +84,23 @@ fn main() -> Result<(), io::Error> {
         vec![9, 10, 11, 12]
     };
 
-    let (air_constraints, lookup_constraints, mut refinable_cols, range_types, general_lookup_info) =
+    let (mut constraint_info, general_lookup_info) =
         extract_constraints_and_range::<KoalaBear, AddSubChip>(&air, NUM_ADD_SUB_COLS, prime);
-    refinable_cols.extend(&output_columns.clone());
+    constraint_info
+        .refinable_cols
+        .extend(&output_columns.clone());
+    constraint_info.output_columns = output_columns.clone();
 
     // ######################## Program Initialization ###########################
     let program = target_program(get_opcode(&opcode_str), 4, 4, 2, 3);
+
+    // ######################## Set Info ##########################################
     let program_info = ProgramInfo {
         program_str: get_program_str(&program),
         program_len: program.instructions.len(),
     };
-
-    // ######################## Search Config ####################################
-    let search_config = SearchConfig::new(refinable_cols.len());
-    let constraints = LatticeVMConstraints::new(air_constraints, lookup_constraints);
-    let mut constraint_info = ConstraintInfo {
-        constraints: constraints,
-        num_total_columns: NUM_ADD_SUB_COLS,
-        num_pv_columns: 0,
-        output_columns: output_columns.clone(),
-        refinable_cols: refinable_cols,
-        range_types: range_types,
-        prime: prime,
-    };
-
-    // ###########################################################################
     let base_abs_main_trace_data = generate_abstract_trace(&program, air_name.to_string(), 1);
+    let search_config = SearchConfig::new(constraint_info.refinable_cols.len());
 
     // ######################## Solve ############################################
     experiment_harness(
@@ -118,7 +109,7 @@ fn main() -> Result<(), io::Error> {
         &search_config,
         &base_abs_main_trace_data,
         vec![],
-        &vec![0],
+        &vec![], // vec![0],
         dummy_program_counter_refine_fn,
         dummy_adjust_pc_program,
         final_check,
--- examples/ziren/src/utils.rs
+++ examples/ziren/src/utils.rs
@@ -15,10 +15,11 @@ use zkm_stark::{
 
 use latticevm::alu::{get_alu_constraint, WordOp};
 use latticevm::interval::AbstractInterval;
+use latticevm::quick::ConstraintInfo;
 use latticevm::solver::{prepare_constraints_and_range_type, RangeType};
 use latticevm::symbolic::{
-    make_impl_constraint, LatticeVMSymbolicEntry, LatticeVMSymbolicExpr as LVSExpr,
-    LatticeVMSymbolicVal,
+    make_impl_constraint, LatticeVMConstraints, LatticeVMSymbolicEntry,
+    LatticeVMSymbolicExpr as LVSExpr, LatticeVMSymbolicVal,
 };
 use latticevm::utils::GeneralLookupInfo;
 
@@ -287,20 +288,14 @@ pub fn extract_constraints_and_range<F, A>(
     air: &A,
     num_cols: usize,
     prime: u32,
-) -> (
-    Vec<LVSExpr>,
-    Vec<LVSExpr>,
-    Vec<usize>,
-    HashMap<usize, RangeType>,
-    GeneralLookupInfo,
-)
+) -> (ConstraintInfo, GeneralLookupInfo)
 where
     F: p3_field::PrimeField32,
     A: Air<LookupBuilder<F>> + Air<SymbolicAirBuilder<F>>,
 {
     let mut u8_cols = vec![];
     let mut multiplicities = HashSet::new();
-    let mut lookup_symbolic_constraints = Vec::new();
+    let mut lookup_constraints = Vec::new();
     let mut received_vars_from_cpu = HashSet::new();
 
     let symbolic_constraints: Vec<SymbolicExpression<F>> =
@@ -311,12 +306,12 @@ where
         ZKM_PROOF_NUM_PV_ELTS,
         &mut u8_cols,
         &mut multiplicities,
-        &mut lookup_symbolic_constraints,
+        &mut lookup_constraints,
         &mut received_vars_from_cpu,
         prime,
     );
 
-    let mut tv_constraints = symbolic_constraints
+    let mut air_constraints = symbolic_constraints
         .iter()
         .map(|sc| convert_p3_expr::<F>(&sc))
         .collect::<Vec<_>>();
@@ -326,16 +321,21 @@ where
         &u8_cols,
         &multiplicities,
         &received_vars_from_cpu,
-        &mut tv_constraints,
-        &lookup_symbolic_constraints,
+        &mut air_constraints,
+        &lookup_constraints,
         prime,
     );
 
-    (
-        tv_constraints,
-        lookup_symbolic_constraints,
-        refinable_cols,
-        range_types,
-        general_lookup_info,
-    )
+    let constraints = LatticeVMConstraints::new(air_constraints, lookup_constraints);
+    let constraint_info = ConstraintInfo {
+        constraints: constraints,
+        num_total_columns: num_cols,
+        num_pv_columns: 0,
+        output_columns: vec![],
+        refinable_cols: refinable_cols,
+        range_types: range_types,
+        prime: prime,
+    };
+
+    (constraint_info, general_lookup_info)
 }
