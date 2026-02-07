89720b27122ec391a587c05be39141b2ca28a239
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-07 18:10:32 -0500

    update

--- examples/ziren/examples/jump.rs
+++ examples/ziren/examples/jump.rs
@@ -25,6 +25,7 @@ use zkm_stark::MachineProver;
 
 use latticevm::interval::AbstractInterval;
 use latticevm::quick::quick_api;
+use latticevm::quick::{experiment_harness, ProgramInfo, SearchConfig};
 use latticevm::smt::expr_to_smt_bv;
 use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn, RangeType};
 use latticevm::ui::save_repr_if_unique;
@@ -94,90 +95,48 @@ fn main() -> Result<(), io::Error> {
     // ######################## Prime and Column Settings ########################
     let prime = 2_u32.pow(31) - 2_u32.pow(24) + 1;
 
-    // ######################## Solver Parameters ###############################
-    let max_iteration = 10000000000000000;
-    let min_row_id = 0;
-    let max_row_id = 0;
-    let num_extracted_rows = 1;
-    let seed = 41;
-
     // ######################## Extract CPU Constraints ##########################
     let air = JumpChip::default();
     let air_name = "Jump";
     let _colmap = make_col_map();
 
-    let (
-        air_constraints,
-        lookup_constraints,
-        mut refinable_cols,
-        mut range_types,
-        general_lookup_info,
-    ) = extract_constraints_and_range::<KoalaBear, JumpChip>(&air, NUM_JUMP_COLS, prime);
-    refinable_cols.extend(&[19, 20, 21, 22, 37, 38, 39, 40]); // output
-    range_types.insert(19, RangeType::U8);
-    range_types.insert(20, RangeType::U8);
-    range_types.insert(21, RangeType::U8);
-    range_types.insert(22, RangeType::U7);
-    range_types.insert(37, RangeType::U8);
-    range_types.insert(38, RangeType::U8);
-    range_types.insert(39, RangeType::U8);
-    range_types.insert(40, RangeType::U7);
+    let (mut constraint_info, general_lookup_info) =
+        extract_constraints_and_range::<KoalaBear, JumpChip>(&air, NUM_JUMP_COLS, prime);
+    let output_columns = vec![19, 20, 21, 22, 37, 38, 39, 40];
 
-    let constraints = LatticeVMConstraints {
-        air_constraints,
-        lookup_constraints,
-        pv_pos_constraints: vec![],
-        pv_neg_constraints: vec![],
-    };
-    let minimum_num_taregt_cols = refinable_cols.len();
+    constraint_info
+        .refinable_cols
+        .extend(&output_columns.clone());
+    constraint_info.output_columns = output_columns.clone();
+    for i in vec![19, 20, 21, 37, 38, 39] {
+        constraint_info.range_types.insert(i, RangeType::U8);
+    }
+    for i in vec![22, 40] {
+        constraint_info.range_types.insert(i, RangeType::U7);
+    }
 
     // ######################## Program Initialization ###########################
     let program = target_program(get_opcode(opcode_str), 4, 4, 1, 32, 1);
-    let base_abs_main_trace_data =
-        generate_abstract_trace(&program, air_name.to_string(), num_extracted_rows);
+    let base_abs_main_trace_data = generate_abstract_trace(&program, air_name.to_string(), 1);
 
-    let mut constants: Vec<(usize, usize, AbstractInterval)> = vec![];
-    let mut neg_constants: Vec<(usize, usize, AbstractInterval)> = vec![];
-    for j in 0..NUM_JUMP_COLS {
-        if !refinable_cols.contains(&j) {
-            constants.push((0, j, base_abs_main_trace_data[0][j].clone()));
-        }
-    }
-    for j in vec![19, 20, 21, 22, 37, 38, 39, 40] {
-        neg_constants.push((0, j, base_abs_main_trace_data[0][j].clone()));
-    }
-
-    let smt_str = expr_to_smt_bv(
-        &constraints,
-        &constants,
-        &neg_constants,
-        &range_types,
-        1,
-        NUM_JUMP_COLS,
-        0,
-        prime,
-    );
-    println!("{}", smt_str);
-    println!("rr: {:?}", range_types);
+    // ######################## Set Info ##########################################
+    let program_info = ProgramInfo {
+        program_str: get_program_str(&program),
+        program_len: program.instructions.len(),
+    };
+    let base_abs_main_trace_data = generate_abstract_trace(&program, air_name.to_string(), 1);
+    let search_config = SearchConfig::new(constraint_info.refinable_cols.len());
 
     // ######################## Solve ############################################
-    quick_api(
-        get_program_str(&program),
-        &constraints,
-        &refinable_cols,
-        &range_types,
-        &vec![],
+    experiment_harness(
+        &program_info,
+        &mut constraint_info,
+        &search_config,
         &base_abs_main_trace_data,
         vec![],
-        max_iteration,
-        minimum_num_taregt_cols,
-        min_row_id,
-        max_row_id,
-        program.instructions.len(),
+        &vec![],
         dummy_program_counter_refine_fn,
         dummy_adjust_pc_program,
         final_check,
-        prime,
-        seed,
     )
 }
