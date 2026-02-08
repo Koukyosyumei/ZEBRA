3251559d9a56bef1ff4b94d0baaef571e0ff6849
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-08 16:51:05 -0500

    update

--- examples/ziren/examples/movcond.rs
+++ examples/ziren/examples/movcond.rs
@@ -3,7 +3,6 @@ use core::mem::transmute;
 use std::collections::HashSet;
 use std::fs;
 use std::io;
-use std::mem::transmute;
 
 use p3_koala_bear::KoalaBear;
 
@@ -11,9 +10,8 @@ use zkm_core_executor::{Instruction, Opcode, Program};
 use zkm_core_machine::misc::MovCondCols;
 use zkm_core_machine::misc::NUM_MOV_COND_COLS;
 use zkm_core_machine::MovCondChip;
-use zkm_stark::MachineProver;
 
-use latticevm::quick::{experiment_harness, ProgramInfo, SearchConfig};
+use latticevm::quick::{experiment_harness, load_config, Args, ProgramInfo};
 use latticevm::solver::RangeType;
 use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn};
 use latticevm::symbolic::AbstractTrace;
@@ -29,8 +27,8 @@ use latticevm_ziren::utils::{
 // ############## Final Check Function ##############################
 fn final_check(
     trace: &AbstractTrace,
-    num_trial: usize,
-    prime: u32,
+    _num_trial: usize,
+    _prime: u32,
     known_reprt: &mut HashSet<String>,
     ui: &mut UiState,
 ) {
@@ -76,9 +74,11 @@ pub fn get_opcode(opcode_str: &str) -> Opcode {
 }
 
 fn main() -> Result<(), io::Error> {
-    let opcode_str = "MEQ";
-
     create_or_clear_dir("voutput")?;
+
+    let args = Args::parse();
+    let opcode_str = args.opcode_str;
+    let mut search_config = load_config(&args.config).unwrap();
 
     // ######################## Prime and Column Settings ########################
     let prime = 2_u32.pow(31) - 2_u32.pow(24) + 1;
@@ -88,7 +88,7 @@ fn main() -> Result<(), io::Error> {
     let air_name = "MovCond";
     let _colmap = make_col_map();
 
-    let (mut constraint_info, general_lookup_info) =
+    let (mut constraint_info, _general_lookup_info) =
         extract_constraints_and_range::<KoalaBear, MovCondChip>(&air, NUM_MOV_COND_COLS, prime);
     let output_columns = vec![2, 3, 4, 5];
 
@@ -101,7 +101,7 @@ fn main() -> Result<(), io::Error> {
     }
 
     // ######################## Program Initialization ###########################
-    let program = target_program(get_opcode(opcode_str), 4, 4, 11, 12, 13);
+    let program = target_program(get_opcode(&opcode_str), 4, 4, 11, 12, 13);
 
     // ######################## Set Info ##########################################
     let program_info = ProgramInfo {
@@ -109,10 +109,12 @@ fn main() -> Result<(), io::Error> {
         program_len: program.instructions.len(),
     };
     let base_abs_main_trace_data = generate_abstract_trace(&program, air_name.to_string(), 1);
-    let search_config = SearchConfig::new(constraint_info.refinable_cols.len());
+    if search_config.minimum_num_taregt_cols == 0 {
+        search_config.minimum_num_taregt_cols = constraint_info.refinable_cols.len();
+    }
 
     // ######################## Solve ############################################
-    experiment_harness(
+    let result = experiment_harness(
         &program_info,
         &mut constraint_info,
         &search_config,
@@ -122,5 +124,9 @@ fn main() -> Result<(), io::Error> {
         dummy_program_counter_refine_fn,
         dummy_adjust_pc_program,
         final_check,
-    )
+        &args.method,
+    );
+    println!("{:?}", result);
+
+    Ok(())
 }
