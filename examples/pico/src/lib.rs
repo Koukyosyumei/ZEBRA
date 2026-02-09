9ad41e6078d72352eb8a886006a06cc23800d5dc
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-09 05:29:04 -0500

    update

--- examples/pico/src/lib.rs
+++ examples/pico/src/lib.rs
@@ -1,3 +1,2 @@
-pub mod lookup;
 pub mod p3_to_tv;
 pub mod utils;
--- examples/pico/src/utils.rs
+++ examples/pico/src/utils.rs
@@ -1,14 +1,14 @@
 use std::collections::HashSet;
 use std::sync::Arc;
 
-use p3_air::Air;
+use p3_air::{Air, PairCol, VirtualPairCol};
 use p3_field::PrimeField32;
 use p3_koala_bear::KoalaBear;
 use p3_uni_stark::SymbolicExpression;
 
 use pico_vm::{
     chips::chips::public_values::columns::NUM_PUBLIC_VALUES_COLS,
-    compiler::riscv::program::Program,
+    compiler::riscv::{opcode::Opcode, program::Program},
     configs::config::StarkGenericConfig,
     emulator::opts::EmulatorOpts,
     emulator::riscv::emulator::RiscvEmulator,
@@ -16,22 +16,24 @@ use pico_vm::{
         chiptype::riscv_chiptype::RiscvChipType,
         configs::embed_kb_bn254_poseidon2::KoalaBearBn254Poseidon2, machine::riscv::RiscvMachine,
     },
-    iter::PicoIterator,
     machine::{
-        chip::ChipBehavior, folder::SymbolicConstraintFolder, machine::MachineBehavior,
+        folder::SymbolicConstraintFolder, lookup::LookupType, machine::MachineBehavior,
         utils::get_symbolic_constraints,
     },
     primitives::consts::RISCV_NUM_PVS,
 };
 
+use latticevm::alu::{get_alu_constraint, WordOp};
 use latticevm::interval::AbstractInterval;
 use latticevm::quick::ConstraintInfo;
 use latticevm::solver::prepare_constraints_and_range_type;
-use latticevm::symbolic::LatticeVMConstraints;
+use latticevm::symbolic::{
+    make_impl_constraint, LatticeVMConstraints, LatticeVMSymbolicExpr as LVSExpr,
+};
 use latticevm::utils::GeneralLookupInfo;
 
-use crate::lookup::get_symbolic_lookup_constraints;
 use crate::p3_to_tv::convert_p3_expr;
+use crate::p3_to_tv::convert_p3_virtual_pair_col as cv;
 
 pub fn run_pico_program(program: &Program) -> Vec<(String, Vec<Vec<AbstractInterval>>)> {
     let config = KoalaBearBn254Poseidon2::new();
@@ -91,6 +93,205 @@ pub fn generate_abstract_trace(
     }
 
     base_abs_main_trace_data
+}
+
+pub fn add_single_var_col_if_possible<F: PrimeField32>(
+    b: &VirtualPairCol<F>,
+    u8_cols: &mut Vec<usize>,
+) {
+    if !b.column_weights.is_empty() {
+        if let p3_air::PairCol::Main(col_idx) = b.column_weights[0].0 {
+            u8_cols.push(col_idx);
+        }
+    }
+}
+
+pub fn add_u16_col_if_possible<F: PrimeField32>(b: &VirtualPairCol<F>, u8_cols: &mut Vec<usize>) {
+    if !b.column_weights.is_empty() {
+        if let p3_air::PairCol::Main(col_idx) = b.column_weights[0].0 {
+            u8_cols.push(col_idx);
+        }
+    }
+}
+
+pub fn get_symbolic_lookup_constraints<F, A>(
+    air: &A,
+    preprocessed_width: usize,
+    _num_public_values: usize,
+    u8_cols: &mut Vec<usize>,
+    multiplicities: &mut HashSet<usize>,
+    lookup_constraints: &mut Vec<LVSExpr>,
+    received_vars_from_cpu: &mut HashSet<usize>,
+    prime: u32,
+) -> GeneralLookupInfo
+where
+    F: p3_field::PrimeField32,
+    A: Air<SymbolicConstraintFolder<F>>,
+{
+    let mut general_lookup_info = GeneralLookupInfo::default();
+    let mut builder = SymbolicConstraintFolder::new(preprocessed_width, air.width());
+    air.eval(&mut builder);
+
+    let (sends, receives) = builder.lookups();
+
+    for r in &receives {
+        for (w, _) in &r.mult.column_weights {
+            if let p3_air::PairCol::Main(col_idx) = w {
+                multiplicities.insert(*col_idx);
+            }
+        }
+    }
+
+    for r in &receives {
+        match r.kind {
+            LookupType::Memory => {}
+            LookupType::Alu => {
+                for rv in &r.values {
+                    for c in &rv.column_weights {
+                        if let PairCol::Main(index) = c.0 {
+                            received_vars_from_cpu.insert(index);
+                        }
+                    }
+                }
+
+                for i in 1..13 {
+                    add_single_var_col_if_possible(&r.values[i], u8_cols);
+                }
+
+                for i in 1..5 {
+                    add_single_var_col_if_possible(
+                        &r.values[i],
+                        &mut general_lookup_info.alu_output,
+                    );
+                }
+                for i in 5..9 {
+                    add_single_var_col_if_possible(
+                        &r.values[i],
+                        &mut general_lookup_info.alu_input1,
+                    );
+                }
+                for i in 9..13 {
+                    add_single_var_col_if_possible(
+                        &r.values[i],
+                        &mut general_lookup_info.alu_input2,
+                    );
+                }
+            }
+            _ => {}
+        }
+    }
+
+    for s in &sends {
+        match s.kind {
+            LookupType::Memory => {}
+            LookupType::Alu => {
+                let multiplicities = cv(&s.mult);
+                let opcode = cv(&s.values[0]);
+                let a = [
+                    cv(&s.values[1]),
+                    cv(&s.values[2]),
+                    cv(&s.values[3]),
+                    cv(&s.values[4]),
+                ];
+                let b = [
+                    cv(&s.values[5]),
+                    cv(&s.values[6]),
+                    cv(&s.values[7]),
+                    cv(&s.values[8]),
+                ];
+                let c = [
+                    cv(&s.values[9]),
+                    cv(&s.values[10]),
+                    cv(&s.values[11]),
+                    cv(&s.values[12]),
+                ];
+
+                let tmps = vec![
+                    (Opcode::ADD as u8, WordOp::Add),
+                    (Opcode::SUB as u8, WordOp::SubU),
+                    (Opcode::MUL as u8, WordOp::Mul),
+                    (Opcode::MULH as u8, WordOp::MulH),
+                    (Opcode::MULHU as u8, WordOp::MulHU),
+                    (Opcode::SLT as u8, WordOp::SLt),
+                    (Opcode::SLTU as u8, WordOp::SLt),
+                    (Opcode::AND as u8, WordOp::And),
+                    (Opcode::OR as u8, WordOp::Or),
+                    (Opcode::XOR as u8, WordOp::Xor),
+                    (Opcode::SRL as u8, WordOp::SRL),
+                ];
+                for t in tmps {
+                    let alu_constraint = get_alu_constraint(&a, &b, &c, &a, &t.1);
+                    let impl_constraint =
+                        make_impl_constraint(t.0 as i64, &opcode, alu_constraint, prime);
+
+                    if let Some(impl_constraint) = impl_constraint {
+                        lookup_constraints.push(LVSExpr::Mul(
+                            Box::new(multiplicities.clone()),
+                            Box::new(impl_constraint),
+                        ));
+                    }
+                }
+            }
+            LookupType::Byte => {
+                let opcode = &s.values[0];
+                let a1 = &s.values[1];
+                let a2 = &s.values[2];
+                let b = &s.values[3];
+                let c = &s.values[4];
+
+                if opcode.column_weights.is_empty() {
+                    if opcode.constant.as_canonical_u32() == 7 {
+                        add_single_var_col_if_possible(&b, u8_cols);
+                        add_single_var_col_if_possible(&c, u8_cols);
+                    }
+
+                    if opcode.constant.as_canonical_u32() == 8 {
+                        add_u16_col_if_possible(&a1, u8_cols);
+                    }
+                }
+
+                let a1_expr = cv(&a1);
+                let b_e = cv(&b);
+                let c_e = cv(&c);
+                let opcode_condition = cv(&opcode);
+
+                let ops = [
+                    (
+                        0,
+                        LVSExpr::And(Box::new(b_e.clone()), Box::new(c_e.clone())),
+                    ),
+                    (1, LVSExpr::Or(Box::new(b_e.clone()), Box::new(c_e.clone()))),
+                    (
+                        2,
+                        LVSExpr::Xor(Box::new(b_e.clone()), Box::new(c_e.clone())),
+                    ),
+                    (
+                        5,
+                        LVSExpr::Flip(Box::new(LVSExpr::Lt(
+                            Box::new(b_e.clone()),
+                            Box::new(c_e.clone()),
+                        ))), // lt(b, c) on abstractinterval returns 0 when b < c
+                    ),
+                    (6, LVSExpr::Msb(Box::new(b_e.clone()))),
+                ];
+
+                for (opcode, op_expr) in ops {
+                    let el_constraint = make_impl_constraint(
+                        opcode,
+                        &opcode_condition,
+                        LVSExpr::Sub(Box::new(a1_expr.clone()), Box::new(op_expr)),
+                        prime,
+                    );
+                    if let Some(el_constraint) = el_constraint {
+                        lookup_constraints.push(el_constraint);
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    general_lookup_info
 }
 
 pub fn extract_constraints_and_range<F, A>(
