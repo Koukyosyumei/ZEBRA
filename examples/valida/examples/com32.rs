c872f8fab97d55f81bce6f0710c341780c1083f8
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-09 07:08:21 -0500

    update

--- examples/valida/examples/com32.rs
+++ examples/valida/examples/com32.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use std::collections::HashSet;
-use std::fs;
 use std::io;
 
 use p3_baby_bear::BabyBear;
@@ -17,10 +16,8 @@ use valida_machine::{Instruction, InstructionWord, Operands, StarkField};
 use valida_opcodes::BYTES_PER_INSTR;
 
 use latticevm::quick::{experiment_harness, load_config, Args, ProgramInfo};
-use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn, RangeType};
+use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn};
 use latticevm::symbolic::AbstractTrace;
-use latticevm::symbolic::LatticeVMConstraints;
-use latticevm::ui::generate_alu_final_checker;
 use latticevm::ui::save_repr_if_unique;
 use latticevm::ui::UiState;
 use latticevm::utils::create_or_clear_dir;
