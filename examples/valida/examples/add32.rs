a6397882f50c24d825bfd3cdadd893644934b753
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-09 07:04:12 -0500

    update

--- examples/valida/examples/add32.rs
+++ examples/valida/examples/add32.rs
@@ -71,6 +71,7 @@ fn main() -> Result<(), io::Error> {
     constraint_info
         .refinable_cols
         .extend(&general_lookup_info.alu_output);
+    constraint_info.output_columns = general_lookup_info.alu_output;
 
     // ######################## Program Initialization ###########################
     let program = get_target_program::<BabyBear>(3, 4);
