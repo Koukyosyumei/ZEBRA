381b8d5686eec4f1badce5b7fe8b1f7e54916a13
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-09 06:54:06 -0500

    update

--- examples/valida/examples/add32.rs
+++ examples/valida/examples/add32.rs
@@ -14,7 +14,6 @@ use valida_opcodes::BYTES_PER_INSTR;
 
 use latticevm::quick::{experiment_harness, load_config, Args, ProgramInfo};
 use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn};
-use latticevm::symbolic::LatticeVMConstraints;
 use latticevm::ui::generate_alu_final_checker;
 use latticevm::utils::create_or_clear_dir;
 
