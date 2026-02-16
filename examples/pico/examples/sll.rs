98be2eda7aa515c61f27f87a829d509df8ce7177
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-16 16:27:53 -0500

    update

--- examples/pico/examples/sll.rs
+++ examples/pico/examples/sll.rs
@@ -49,8 +49,8 @@ fn main() -> Result<(), io::Error> {
     let final_check = generate_alu_final_checker(general_lookup_info.clone());
     constraint_info
         .refinable_cols
-        .extend(&general_lookup_info.alu_output);
-    constraint_info.output_columns = general_lookup_info.alu_output;
+        .extend(&general_lookup_info.op_a);
+    constraint_info.output_columns = general_lookup_info.op_a;
 
     // ######################## Program Initialization ###########################
     let program = target_program(4, 4, 1, 2);
