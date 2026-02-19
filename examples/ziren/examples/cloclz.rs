c266549771a4e7dee747d38612bd883aa3c76d01
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-19 11:35:21 -0500

    update

--- examples/ziren/examples/cloclz.rs
+++ examples/ziren/examples/cloclz.rs
@@ -89,8 +89,8 @@ fn main() -> Result<(), io::Error> {
     let mut rng = StdRng::seed_from_u64(search_config.seed);
     let mut ds = vec![];
     for _ in 0..30 {
-        let x: u32 = rng.random();
-        let y: u32 = rng.random();
+        let x: u32 = rng.random_range(0..256);
+        let y: u32 = rng.random_range(0..256);
 
         // ######################## Program Initialization ###########################
         let program = target_program(get_opcode(&opcode_str), 4, 4, x, y);
