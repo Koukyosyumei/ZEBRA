92ab0b2f708c0169a9b1199eb891443e385528a2
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-22 16:20:01 -0500

    update

--- examples/ziren/tests/test_cpu.rs
+++ examples/ziren/tests/test_cpu.rs
@@ -85,6 +85,40 @@ mod tests {
     }
 
     #[test]
+    fn test_branch_bgez_zero() {
+        fn target_program(pc_start: u32, pc_base: u32) -> Program {
+            let instructions = vec![
+                Instruction::new(Opcode::ADD, 1, 0, 0, true, true),
+                Instruction::new(Opcode::BGEZ, 1, 0, 6, false, false),
+            ];
+
+            Program::new(instructions, pc_start, pc_base)
+        }
+
+        let air = CpuChip::default();
+        let (constraint_info, _general_lookup_info) =
+            extract_constraints_and_range::<KoalaBear, CpuChip>(&air, NUM_CPU_COLS, PRIME);
+
+        // ######################## Program Initialization ###########################
+        let program = target_program(4, 4);
+        let base_abs_main_trace_data = generate_abstract_trace(&program, "Cpu".to_string(), 5);
+
+        // ######################## Public Values ####################################
+        let mut public_vals = vec![AbstractInterval::zero(); ZKM_PROOF_NUM_PV_ELTS];
+        public_vals[40] = AbstractInterval::from_i64(4);
+        public_vals[41] = AbstractInterval::from_i64(12);
+        public_vals[44] = AbstractInterval::one();
+
+        let mut at = AbstractTrace::new(base_abs_main_trace_data);
+        let result = eval_constraints(&at, Some(&public_vals), &constraint_info.constraints, PRIME);
+        assert!(result.0 == MayBeFlag::True);
+
+        at.data[1][7] = AbstractInterval::from_i64(16);
+        let result = eval_constraints(&at, Some(&public_vals), &constraint_info.constraints, PRIME);
+        assert!(result.0 == MayBeFlag::False);
+    }
+
+    #[test]
     fn test_branch_bgez_negative() {
         fn target_program(pc_start: u32, pc_base: u32) -> Program {
             let instructions = vec![
