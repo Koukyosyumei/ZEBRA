4eb85028e3f436058d1c169d1ebad034747ae966
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-22 16:56:14 -0500

    update

--- examples/ziren/tests/test_cpu.rs
+++ examples/ziren/tests/test_cpu.rs
@@ -331,4 +331,42 @@ mod tests {
         let result = eval_constraints(&at, Some(&public_vals), &constraint_info.constraints, PRIME);
         assert!(result.0 == MayBeFlag::False);
     }
+
+    #[test]
+    fn test_branch_jumpdirect() {
+        println!("{:?}", CPU_COL_MAP);
+
+        fn target_program(pc_start: u32, pc_base: u32) -> Program {
+            let instructions = vec![
+                Instruction::new(Opcode::ADD, 3, 123, 0, true, true),
+                Instruction::new(Opcode::JumpDirect, 2, 16, 0, true, true), // if imm_b is true, it cannot pass the constraints
+            ];
+
+            Program::new(instructions, pc_start, pc_base)
+        }
+
+        let air = CpuChip::default();
+        let (constraint_info, _general_lookup_info) =
+            extract_constraints_and_range::<KoalaBear, CpuChip>(&air, NUM_CPU_COLS, PRIME);
+
+        // ######################## Program Initialization ###########################
+        let program = target_program(4, 4);
+        let base_abs_main_trace_data = generate_abstract_trace(&program, "Cpu".to_string(), 5);
+        assert!(base_abs_main_trace_data[1][7] == AbstractInterval::from_i64(28));
+
+        // ######################## Public Values ####################################
+        let mut public_vals = vec![AbstractInterval::zero(); ZKM_PROOF_NUM_PV_ELTS];
+        public_vals[40] = AbstractInterval::from_i64(4);
+        public_vals[41] = AbstractInterval::from_i64(12);
+        public_vals[44] = AbstractInterval::one();
+
+        let mut at = AbstractTrace::new(base_abs_main_trace_data);
+        println!("{}", at);
+        let result = eval_constraints(&at, Some(&public_vals), &constraint_info.constraints, PRIME);
+        assert!(result.0 == MayBeFlag::True);
+
+        at.data[1][7] = AbstractInterval::from_i64(16);
+        let result = eval_constraints(&at, Some(&public_vals), &constraint_info.constraints, PRIME);
+        assert!(result.0 == MayBeFlag::False);
+    }
 }
