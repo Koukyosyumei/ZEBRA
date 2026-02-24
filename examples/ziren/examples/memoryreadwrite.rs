5f0415880e6a82aa9b429a627106f3c158640f1b
Koukyosyumei <koukyosyumei@hotmail.com>
2026-02-24 18:24:22 -0500

    update

--- examples/ziren/examples/memoryreadwrite.rs
+++ examples/ziren/examples/memoryreadwrite.rs
@@ -1,7 +1,6 @@
 use clap::Parser;
 use core::mem::transmute;
-use std::collections::HashSet;
-use std::fs;
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use std::io;
 
 use itertools::Itertools;
@@ -14,13 +13,17 @@ use zkm_core_machine::memory::{
 };
 use zkm_core_machine::MemoryInstructionsChip;
 
-use latticevm::quick::{experiment_harness, load_config, Args, ProgramInfo};
+use latticevm::canonicalizer::{generate_memory_op_final_checker, save_repr_if_unique};
+use latticevm::quick::{
+    experiment_harness, generate_report, load_config, write_output, Args, ProgramInfo,
+};
 use latticevm::solver::{dummy_adjust_pc_program, dummy_program_counter_refine_fn};
-use latticevm::ui::save_repr_if_unique;
 use latticevm::ui::UiState;
-use latticevm::utils::trace_fmt_with_idxs;
 use latticevm::utils::{create_or_clear_dir, indices_arr};
-use latticevm::{symbolic::AbstractTrace, symbolic::LatticeVMConstraints};
+use latticevm::{
+    constraint::LatticeVMConstraints,
+    trace::{trace_fmt_with_idxs, AbstractTrace},
+};
 
 use latticevm_ziren::utils::{
     extract_constraints_and_range, generate_abstract_trace, get_program_str,
@@ -73,7 +76,7 @@ fn main() -> Result<(), io::Error> {
     create_or_clear_dir("voutput")?;
 
     let args = Args::parse();
-    let opcode_str = args.opcode_str;
+    let opcode_str = args.opcode_str.clone();
     let mut search_config = load_config(&args.config).unwrap();
     let (opcode, is_load) = get_opcode(&opcode_str);
 
@@ -112,43 +115,57 @@ fn main() -> Result<(), io::Error> {
     constraint_info
         .refinable_cols
         .retain(|c| !semantic_inputs.contains(c));
-
-    // ######################## Program Initialization ###########################
-    let program = if is_load {
-        target_program_load(opcode, 4, 4)
-    } else {
-        target_program_store(opcode, 4, 4)
-    };
-    let num_extracted_rows = if is_load { 2 } else { 1 };
-    let base_abs_main_trace_data =
-        generate_abstract_trace(&program, air_name.to_string(), num_extracted_rows);
-
-    // ######################## Set Info ##########################################
-    let program_info = ProgramInfo {
-        program_str: get_program_str(&program),
-        program_len: program.instructions.len(),
-    };
-
     search_config.min_row_id = if is_load { 1 } else { 0 };
     search_config.max_row_id = if is_load { 1 } else { 0 };
     if search_config.minimum_num_taregt_cols == 0 {
         search_config.minimum_num_taregt_cols = 3; //constraint_info.refinable_cols.len();
     }
 
-    // ######################## Solve ############################################
-    let result = experiment_harness(
-        &program_info,
-        &mut constraint_info,
-        &search_config,
-        &base_abs_main_trace_data,
-        vec![],
-        &vec![], // vec![0],
-        dummy_program_counter_refine_fn,
-        dummy_adjust_pc_program,
-        final_check,
-        &args.method,
-    );
-    println!("{:?}", result);
+    let mut rng = StdRng::seed_from_u64(search_config.seed);
+    let mut ds = vec![];
+    for i in 0..30 {
+        search_config.seed += i;
+
+        let r1: u8 = rng.random_range(0..32);
+        let r2: u8 = rng.random_range(0..32);
+        let x: u32 = rng.random();
+        let y: u32 = rng.random_range(0..32513);
+        let z: u32 = rng.random_range(0..32513);
+
+        // ######################## Program Initialization ###########################
+        let program = if is_load {
+            target_program_load(opcode, 4, 4)
+        } else {
+            target_program_store(opcode, 4, 4)
+        };
+        let num_extracted_rows = if is_load { 2 } else { 1 };
+        let base_abs_main_trace_data =
+            generate_abstract_trace(&program, air_name.to_string(), num_extracted_rows);
+
+        // ######################## Set Info ##########################################
+        let program_info = ProgramInfo {
+            program_str: get_program_str(&program),
+            program_len: program.instructions.len(),
+        };
+
+        // ######################## Solve ############################################
+        let result = experiment_harness(
+            &program_info,
+            &mut constraint_info,
+            &search_config,
+            &base_abs_main_trace_data,
+            vec![],
+            &vec![], // vec![0],
+            dummy_adjust_pc_program,
+            &final_check,
+            &args.method,
+        );
+        println!("({} {} {} {} {}), {:?}", r1, r2, x, y, z, result);
+        ds.push(result.unwrap());
+    }
+    let report = generate_report(&ds);
+    println!("{:?}", report);
+    let _ = write_output(args, search_config, report);
 
     Ok(())
 }
