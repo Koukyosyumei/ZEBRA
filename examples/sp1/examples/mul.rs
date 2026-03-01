8992f667ad7dd2460cbb6b3986578a39716be5fe
Koukyosyumei <koukyosyumei@hotmail.com>
2026-03-01 15:04:19 -0500

    update

--- examples/sp1/examples/mul.rs
+++ examples/sp1/examples/mul.rs
@@ -11,14 +11,11 @@ use sp1_core_machine::alu::NUM_MUL_COLS;
 use sp1_core_machine::riscv::MulChip;
 
 use latticevm::canonicalizer::generate_alu_final_checker;
-use latticevm::constraint::eval_constraints;
 use latticevm::quick::{
     experiment_harness, generate_report, load_config, write_output, Args, ProgramInfo,
 };
-use latticevm::solver::make_init_val;
 use latticevm::solver::RangeType;
-use latticevm::solver::{dummy_program_counter_refine_fn, nop_post_process};
-use latticevm::trace::AbstractTrace;
+use latticevm::solver::nop_post_process;
 use latticevm::utils::{create_or_clear_dir, indices_arr};
 
 use latticevm_sp1::utils::{
@@ -86,7 +83,7 @@ fn main() -> Result<(), io::Error> {
 
         // ######################## Program Initialization ###########################
         let program = target_program(get_opcode(&opcode_str), 4, 4, x, y);
-        let mut base_abs_main_trace_data =
+        let base_abs_main_trace_data =
             generate_abstract_trace(&program, air_name.to_string(), 1);
 
         // ######################## Set Info ##########################################
