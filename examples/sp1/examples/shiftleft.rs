1b671efacb4b16b65aba016295f511f2047742d6
Koukyosyumei <koukyosyumei@hotmail.com>
2026-03-01 15:03:57 -0500

    update

--- examples/sp1/examples/shiftleft.rs
+++ examples/sp1/examples/shiftleft.rs
@@ -14,7 +14,7 @@ use latticevm::canonicalizer::generate_alu_final_checker;
 use latticevm::quick::{
     experiment_harness, generate_report, load_config, write_output, Args, ProgramInfo,
 };
-use latticevm::solver::{dummy_program_counter_refine_fn, nop_post_process};
+use latticevm::solver::nop_post_process;
 use latticevm::utils::{create_or_clear_dir, indices_arr};
 
 use latticevm_sp1::utils::{
