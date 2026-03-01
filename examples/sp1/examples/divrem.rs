736230e419bb5a5a3a4e547508100db7d4dba1ca
Koukyosyumei <koukyosyumei@hotmail.com>
2026-03-01 15:09:27 -0500

    update

--- examples/sp1/examples/divrem.rs
+++ examples/sp1/examples/divrem.rs
@@ -1,7 +1,6 @@
 use clap::Parser;
 use core::mem::transmute;
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use std::collections::HashSet;
 use std::io;
 
 use p3_baby_bear::BabyBear;
@@ -11,14 +10,10 @@ use sp1_core_machine::alu::{DivRemCols, NUM_DIVREM_COLS};
 use sp1_core_machine::riscv::DivRemChip;
 
 use latticevm::canonicalizer::generate_alu_final_checker;
-use latticevm::canonicalizer::save_repr_if_unique;
-use latticevm::constraint::LatticeVMConstraints;
 use latticevm::quick::{
     experiment_harness, generate_report, load_config, write_output, Args, ProgramInfo,
 };
-use latticevm::solver::{dummy_program_counter_refine_fn, nop_post_process, RangeType};
-use latticevm::trace::{trace_fmt_with_idxs, AbstractTrace};
-use latticevm::ui::UiState;
+use latticevm::solver::nop_post_process;
 use latticevm::utils::{create_or_clear_dir, indices_arr};
 
 use latticevm_sp1::utils::{
