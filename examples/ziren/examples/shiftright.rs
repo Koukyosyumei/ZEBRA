caa91ac43ff83f9a0f9fc0bef16b54d717d7d052
Koukyosyumei <koukyosyumei@hotmail.com>
2026-03-22 20:57:03 -0400

    update

---
+++ examples/ziren/examples/shiftright.rs
@@ -0,0 +1,98 @@
+use clap::Parser;
+use core::mem::transmute;
+use p3_koala_bear::KoalaBear;
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use std::collections::HashSet;
+use std::io;
+
+use zkm_core_executor::{Instruction, Opcode, Program};
+use zkm_core_machine::alu::{ShiftRightCols, NUM_SHIFT_RIGHT_COLS};
+use zkm_core_machine::ShiftRightChip;
+
+use zebra::canonicalizer::generate_alu_final_checker;
+use zebra::quick::{
+    experiment_harness, generate_report, load_config, write_output, Args, ProgramInfo,
+};
+use zebra::solver::nop_post_process;
+use zebra::utils::{create_or_clear_dir, indices_arr};
+
+use zebra_ziren::utils::{
+    extract_constraints_and_range, generate_abstract_trace, get_program_str,
+};
+
+const fn make_col_map() -> ShiftRightCols<usize> {
+    let indices_arr = indices_arr::<{ NUM_SHIFT_RIGHT_COLS }>();
+    unsafe { transmute::<[usize; NUM_SHIFT_RIGHT_COLS], ShiftRightCols<usize>>(indices_arr) }
+}
+
+pub fn target_program(pc_start: u32, pc_base: u32, x: u32, y: u32) -> Program {
+    let instructions = vec![Instruction::new(Opcode::SRL, 1, x, y, true, true)];
+    Program::new(instructions, pc_start, pc_base)
+}
+
+fn main() -> Result<(), io::Error> {
+    create_or_clear_dir("voutput")?;
+
+    let args = Args::parse();
+    let _opcode_str = args.opcode_str.clone();
+    let mut search_config = load_config(&args.config).unwrap();
+
+    // ######################## Prime and Column Settings ########################
+    let prime = 2_u32.pow(31) - 2_u32.pow(24) + 1;
+
+    // ######################## Extract CPU Constraints ##########################
+    let air = ShiftRightChip::default();
+    let air_name = "ShiftRight";
+    let _colmap = make_col_map();
+
+    let (mut constraint_info, general_lookup_info) =
+        extract_constraints_and_range::<KoalaBear, ShiftRightChip>(&air, NUM_SHIFT_RIGHT_COLS, prime);
+    let final_check = generate_alu_final_checker(general_lookup_info.clone());
+
+    constraint_info
+        .refinable_cols
+        .extend(&general_lookup_info.op_a.clone());
+    constraint_info.output_columns = general_lookup_info.op_a.clone();
+    if search_config.minimum_num_taregt_cols == 0 {
+        search_config.minimum_num_taregt_cols = constraint_info.refinable_cols.len();
+    }
+
+    let mut rng = StdRng::seed_from_u64(search_config.seed);
+    let mut ds = vec![];
+    for _ in 0..args.num_trial {
+        let x: u32 = rng.random();
+        let y: u32 = rng.random();
+
+        // ######################## Program Initialization ###########################
+        let program = target_program(4, 4, x, y);
+        let base_abs_main_trace_data = generate_abstract_trace(&program, air_name.to_string(), 1);
+
+        // ######################## Set Info ##########################################
+        let program_info = ProgramInfo {
+            program_str: get_program_str(&program),
+            program_len: program.instructions.len(),
+        };
+
+        // ######################## Solve ############################################
+        let mut known_solution = HashSet::new();
+        let result = experiment_harness(
+            &program_info,
+            &mut constraint_info,
+            &search_config,
+            &base_abs_main_trace_data,
+            vec![],
+            &vec![], // vec![0],
+            nop_post_process,
+            &final_check,
+            &args.method,
+            &mut known_solution,
+        );
+        println!("({} {}), {:?}", x, y, result);
+        ds.push(result.unwrap());
+    }
+    let report = generate_report(&ds);
+    println!("{:?}", report);
+    let _ = write_output(args, search_config, report);
+
+    Ok(())
+}
