0b3c788a6a723090955e7aa64e2627c44362292c
Koukyosyumei <koukyosyumei@hotmail.com>
2026-04-15 13:17:03 -0400

    update

--- src/quick.rs
+++ src/quick.rs
@@ -382,17 +382,14 @@ pub fn generate_report(results: &[VerificationResult]) -> ResultReport {
         .iter()
         .map(|d| d.execution_time.as_secs_f64())
         .collect();
-    let mut ys = xs.clone();
-    ys.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let trim_n = (n as f64 * 0.05) as usize;
-    let trimmed = &ys[..(xs.len() - trim_n)];
+    //let mut ys = xs.clone();
+    //ys.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    //let trim_n = (n as f64 * 0.05) as usize;
+    //let trimmed = &ys[..(xs.len() - trim_n)];
 
-    let exe_time_mean = trimmed.iter().sum::<f64>() / (trim_n as f64);
-    let exe_time_variance = trimmed
-        .iter()
-        .map(|x| (x - exe_time_mean).powi(2))
-        .sum::<f64>()
-        / (trim_n as f64);
+    let exe_time_mean = xs.iter().sum::<f64>() / (xs.len() as f64);
+    let exe_time_variance =
+        xs.iter().map(|x| (x - exe_time_mean).powi(2)).sum::<f64>() / (xs.len() as f64);
 
     ResultReport {
         success_ratio,
